44d4269ff221ef4b5e173ef26ebaba613816dd87
emekah <peacewillemeka@gmail.com>
2024-11-18 14:16:36 +0000

    made footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { footerLinks, footerBottom } from "./constants";
 const Footer = () => {
   return (
     <section className=" bg-black">
-      <div className="flex justify-between p-10 text-sm ">
-        <div className="flex gap-60">
+      <div className="md:flex mx-auto justify-between p-10 text-sm max-lg:flex-col">
+        <div className="flex gap-60 max-sm:flex-col">
           {footerLinks.map((footerLink) => (
             // eslint-disable-next-line react/jsx-key
             <div className="">
@@ -23,17 +23,17 @@ const Footer = () => {
             </div>
           ))}
         </div>
-        <div className="flex text-white">
+        <div className="flex text-white max-sm:mt-8">
           <div>
             <BiWorld />
           </div>
           <div>
-            <p className="ml-1 leading-none">United Kingdom</p>
+            <p className="ml-1 leading-none ">United Kingdom</p>
           </div>
         </div>
       </div>
 
-      <div className="flex gap-10 text-sm text-slate-500 p-10">
+      <div className="flex gap-10 text-sm text-slate-500 p-10 max-sm:flex-col-reverse">
         <p className="leading-normal">© 2024 Nike, Inc. All rights reserved</p>
         {footerBottom.map((url) => (
           <p className="leading-normal cursor-pointer" key={url.name}>
